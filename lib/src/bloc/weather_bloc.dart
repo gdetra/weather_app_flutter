0f7b089666a835a9c415584e6916ef8e4be06aa4
Gabriele De trane <detranegabriele@gmail.com>
2019-01-30 19:11:19 +0100

    Nothing changed

--- lib/src/bloc/weather_bloc.dart
+++ lib/src/bloc/weather_bloc.dart
@@ -5,10 +5,11 @@ class WeatherBloc {
   final _weatherList = PublishSubject<List<WeatherModel>>();
 
   //Getter for stream
-  Observable<List<WeatherModel> get weatherListStream => _weatherList.stream;
+  Observable<List<WeatherModel>> get weatherListStream => _weatherList.stream;
 
   //Getter for add method
-  Function(List<WeatherModel>) get addListOfWeatherModel => _weatherList.sink.add;
+  Function(List<WeatherModel>) get addListOfWeatherModel =>
+      _weatherList.sink.add;
 
   dispose() {
     _weatherList.close();
