3967927122343fdf51b249fa9c6bdb1050732229
Gabriele De trane <detranegabriele@gmail.com>
2019-01-30 19:13:23 +0100

    Search improved and added connection with stream and bloc

--- lib/src/widgets/search_delegate.dart
+++ lib/src/widgets/search_delegate.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/src/api/api.dart';
 import 'package:weather_app/src/bloc/weather_bloc.dart';
+import 'package:weather_app/src/models/location_model.dart';
 import 'package:weather_app/src/models/weather_model.dart';
 
 class Search extends SearchDelegate<WeatherModel> {
@@ -40,32 +41,77 @@ class Search extends SearchDelegate<WeatherModel> {
   Widget buildResults(BuildContext context) {
     //This method allow you to see on the screen a widget
     //that contains the results of your search
-    return Column(
-      children: <Widget>[
-        ListTile(
-          title: Text(query),
-        ),
-        Divider(),
-      ],
-    );
+    if (!(query.isEmpty || query == null)) {
+      return buildTile(weatherBloc, query);
+    } else {
+      return Container();
+    }
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     //This method allow you to see on the screen a widget
     //that contains all the avaliable suggestions for what are you searching for
+
     if (!(query.isEmpty || query == null)) {
-      return Column(
-        children: <Widget>[
-          ListTile(
-            title: Text(query),
-            contentPadding: EdgeInsets.all(8.0),
-          ),
-          Divider(),
-        ],
-      );
+      return buildTile(weatherBloc, query);
     } else {
       return Container();
     }
+  }
+
+  buildTile(WeatherBloc bloc, String query) {
+    return FutureBuilder(
+      future: api.fetchLocationData(query),
+      builder: (context, AsyncSnapshot<LocationModel> snapshotLocation) {
+        if (!snapshotLocation.hasData) {
+          return ListTile(
+            title: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+
+        if (!(snapshotLocation.data.status == "OK")) {
+          return ListTile(
+            title: Center(
+              child: Text('NO DATA!'),
+            ),
+          );
+        }
+
+        return FutureBuilder(
+          future: api.fetchWeatherData(
+              snapshotLocation.data.results[0].geometry.location.lat,
+              snapshotLocation.data.results[0].geometry.location.lng),
+          builder: (context, AsyncSnapshot<WeatherModel> snapshotWeather) {
+            if (!snapshotWeather.hasData) {
+              return ListTile(
+                title: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            }
+
+            if (!(snapshotWeather.data.cod == "200")) {
+              return ListTile(
+                title: Center(
+                  child: Text('NO DATA!'),
+                ),
+              );
+            }
+
+            return ListTile(
+              title: Text(snapshotLocation.data.results[0].formattedAddress),
+              onTap: () {
+                weatherList.add(snapshotWeather.data);
+                bloc.addListOfWeatherModel(weatherList);
+                close(context, null);
+              },
+            );
+          },
+        );
+      },
+    );
   }
 }
