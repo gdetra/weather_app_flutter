fc8cd0b07865bb751b1210088dd996422f8e6ccd
Gabriele De trane <detranegabriele@gmail.com>
2019-01-30 16:21:20 +0100

    MaterialApp wrapped inside a WeatherProvider

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/src/bloc/weather_provider.dart';
 import 'package:weather_app/src/widgets/home_widget.dart';
 
 class App extends StatefulWidget {
@@ -9,8 +10,10 @@ class App extends StatefulWidget {
 
 class AppState extends State<App> {
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Home(),
+    return WeatherProvider(
+      child: MaterialApp(
+        home: Home(),
+      ),
     );
   }
 }
