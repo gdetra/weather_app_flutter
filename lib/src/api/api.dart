77457568466fdaf26b4388cc3a7eb31a7b3117a0
Gabriele De trane <detranegabriele@gmail.com>
2019-01-30 19:06:58 +0100

    Small changes

--- lib/src/api/api.dart
+++ lib/src/api/api.dart
@@ -10,7 +10,7 @@ class ApiProvider {
   //Initialization of URLs root.
   final _apiMapsUrl =
       'https://maps.googleapis.com/maps/api/geocode/json?address=';
-  final _apiWeatherUrl = 'http://samples.openweathermap.org/data/2.5/weather?';
+  final _apiWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?';
 
   //Initialization of the Api keys.
   //You can get this keys on Google Maps and Openweathermap.org
@@ -20,7 +20,7 @@ class ApiProvider {
   //This method return a Future<WeatherModel> and receive as parameters latitude and longitude
   //It makes a get request to fetch weather data from openweathermap Api
   Future<WeatherModel> fetchWeatherData(double lat, double lon) async {
-    var url = '${_apiWeatherUrl}lat=$lat&lon=$lon&&appid=$_apiKeyWeather';
+    var url = '${_apiWeatherUrl}lat=$lat&lon=$lon&appid=$_apiKeyWeather';
     var response = await client.get(url);
     return WeatherModel.fromJson(json.decode(response.body));
   }
@@ -28,8 +28,7 @@ class ApiProvider {
   //This method return a Future<LocationModel> and receive as parameter an address or a city name
   //It makes a get request to fetch address data from Google Maps Api
   Future<LocationModel> fetchLocationData(String cityName) async {
-    var encodedCityname = cityName.replaceAll(' ', '%20');
-    var url = '$_apiMapsUrl$encodedCityname&key=$_apiKeyMaps';
+    var url = '$_apiMapsUrl${cityName.replaceAll(' ', '%20')}&key=$_apiKeyMaps';
     var response = await client.get(url);
     return LocationModel.fromJson(json.decode(response.body));
   }
