6af37f2cebdac8e18996f9af49e116c3ea394be4
Gabriele De trane <detranegabriele@gmail.com>
2019-01-30 19:08:09 +0100

    Added instance variable weatherList

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/src/bloc/weather_provider.dart';
+import 'package:weather_app/src/models/weather_model.dart';
 import 'package:weather_app/src/widgets/home_widget.dart';
 
 class App extends StatefulWidget {
@@ -9,10 +10,12 @@ class App extends StatefulWidget {
 }
 
 class AppState extends State<App> {
+  List<WeatherModel> weatherList = [];
+
   Widget build(BuildContext context) {
     return WeatherProvider(
       child: MaterialApp(
-        home: Home(),
+        home: Home(weatherList),
       ),
     );
   }
